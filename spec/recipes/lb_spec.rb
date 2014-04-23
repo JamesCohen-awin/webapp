04e8f51c4050f3d5c22130a525e1ade1f0f2719c
James Cohen <james.cohen@affiliatewindow.com>
2014-04-23 22:37:37 +0100

    small test tweaks

--- spec/recipes/lb_spec.rb
+++ spec/recipes/lb_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
+haproxyConfigFile = '/etc/haproxy/haproxy.cfg'
+
 describe 'webapp::lb' do
   let(:chef_run) { ChefSpec::Runner.new(step_into: ["haproxy_lb"]).converge(described_recipe) }
 
@@ -8,19 +10,24 @@ describe 'webapp::lb' do
   end
 
   it 'Creates the HAProxy config file' do
-    expect(chef_run).to create_template '/etc/haproxy/haproxy.cfg'
+    expect(chef_run).to create_template haproxyConfigFile
+  end
+
+  it 'Tells the HAProxy process to reload the config' do
+    resource = chef_run.template(haproxyConfigFile)
+    expect(resource).to notify('service[haproxy]').to(:reload).delayed
   end
 
   it 'Contains the myapp frontend' do
-    expect(chef_run).to render_file('/etc/haproxy/haproxy.cfg').with_content('frontend myapp')
+    expect(chef_run).to render_file(haproxyConfigFile).with_content('frontend myapp')
   end
 
   it 'Contains the myapp backend' do
-    expect(chef_run).to render_file('/etc/haproxy/haproxy.cfg').with_content('backend myapp')
+    expect(chef_run).to render_file(haproxyConfigFile).with_content('backend myapp')
   end
 
   it 'Uses round-robin load balancing' do
-    expect(chef_run).to render_file('/etc/haproxy/haproxy.cfg').with_content('balance roundrobin')
+    expect(chef_run).to render_file(haproxyConfigFile).with_content('balance roundrobin')
   end
 
 end
