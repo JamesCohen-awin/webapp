fdd1d6e746abb105d21b650e883329a4476532a7
James Cohen <james.cohen@affiliatewindow.com>
2014-04-30 21:15:40 +0100

    file renames and moves to get tests working

--- test/integration/devvm/serverspec/devserver_spec.rb
+++ test/integration/devvm/serverspec/devserver_spec.rb
@@ -1,4 +1,14 @@
-require 'spec_helper'
+require 'serverspec'
+
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+RSpec.configure do |c|
+  c.before :all do
+    c.path = '/sbin:/usr/sbin'
+  end
+end
+
 
 # Apache
 describe service('httpd') do
